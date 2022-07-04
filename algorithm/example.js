e660e91af9c2febbc3bd682f8da60f67886adb25
veritakim <kjs3597@gmail.com>
2022-07-04 11:59:40 +0900

    first commit

---
+++ algorithm/example.js
@@ -0,0 +1,15 @@
+const end = 100
+let answer = 0
+
+for ( let i = 1; i < end; i = i + 2) {
+	answer = answer + 1
+}
+
+console.log(answer)
+
+
+const limit = 100
+
+const answer2 = Math.floor( limit / 2 )
+
+console.log( answer2 )
